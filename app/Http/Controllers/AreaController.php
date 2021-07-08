c2eb415bfdeba54072ccaaa295da5cdc95c9d14a
علی عزیزی | Ali Azizi <46823074+aliazizi77@users.noreply.github.com>
2021-07-08 13:50:14 +0430

    add function to area controller and web route

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Area;
 use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except();
+    }
+
+    public function index()
+    {
+        $areas = Area::all();
+        return view('area.index', compact('areas'));
+    }
+
+    public function create()
+    {
+        return view('area.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'group_id' => ['required', 'string', 'size:11'],
+        ]);
+        Area::create([
+            'name' => $request->name,
+            'group_id' => $request->group,
+        ]);
+        return redirect()->route('area.index')->with('success', 'منطقه ' . $request['name'] . ' افزوده شد.');
+    }
+
+    public function getArea()
+    {
+        $areas = Area::all();
+        return response()->json($areas);
+    }
+
+    public function update(Request $request)
+    {
+        Area::find($request->areaId)->update([
+            "name" => $request->name,
+            "group_id" => $request->phone,
+        ]);
+        $area = Area::where('id', $request->areaId)->first();
+        return response()->json($area);
+    }
+
+    public function destroy($id)
+    {
+        Area::destroy($id);
+        return redirect()->route('area.index')->with('success', 'منطقه مورد نظر با موفقیت حذف شد.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/areas', 'AreaController@index')->name('area.index');
+Route::get('/area/create', 'CustomerController@create')->name('area.create');
+Route::post('/areas', 'CustomerController@store')->name('area.store');
+Route::post('/getAreas', 'CustomerController@getArea')->name('get.area');
+Route::post('/area/update', 'CustomerController@update')->name('area.update');
+Route::get('/area/destroy/{id}', 'CustomerController@destroy')->name('area.destroy');
